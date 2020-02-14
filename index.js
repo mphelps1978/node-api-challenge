c89644910bb2cc2fbee1643e7de68d4753bc1255
Michael Phelps <michaeljphelps1978@gmail.com>
2020-02-14 11:48:08 -0600

    Code written, but still having that launch issue

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+const express = require('express');
 
-Sing along:
+const projectRouter = require('./routers/projectRouter');
+const actionRouter = require('./routers/actionRouter');
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
-I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
+const app = express();
 
-Go code!
-*/
+app.use(logger);
+app.use(express.json());
+
+
+app.use('/api/projects', projectRouter);
+app.use('/api/actions', actionRouter);
+
+app.get('/', (req, res) => {
+  res.send(`<h2>Node.js Sprint Challenge!</h2>`);
+});
+
+// Custom Middleware
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+
+  next();
+}
+
+app.listen(port, () => {
+    console.log(`Server started on ${port}`);
+});
